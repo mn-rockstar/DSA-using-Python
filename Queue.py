5f4b9c1e7b77a06fda4bb35fe91e22e2a35b9835
mohit narvariya <mohitnarvariya70@gmail.com>
2025-06-03 20:18:22 +0530

    intitial commit

--- Queue.py
+++ Queue.py
@@ -3,3 +3,20 @@ class Queue:
     def __init__(self):
         self.items = []
     
+    def is_Empty(self):
+        return len(self.items) == 0
+
+    def enque(self,data):
+        self.items.append(data)
+    
+    def deque(self):
+        if not self.is_Empty():
+            self.items.pop(0)
+        else:
+            return "The Queue is Empty"
+    
+    def peek(self):
+        if not self.is_Empty():
+            return self.items[-1]
+        else:
+            return "The Queue is empty"
